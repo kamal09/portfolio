55b630ec444b37865d9ad56d46cb034924417123
kamal_hossain <sony@gmail.com>
2022-12-09 12:49:39 +0600

    first part

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import './custom_style.css';
-
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
     return (
@@ -15,30 +16,37 @@ function App() {
                              alt="Own Image"/>
                     </div>
                     <div className="col-9">
-                        <div className="row">
-                            <div className="col name-div mt-5">MD. KAMAL HOSSAIN</div>
+                        <div className="d-flex flex-column">
+                            <div className="py-2" style={{background: "#418ab3"}}>MD. KAMAL HOSSAIN</div>
                         </div>
-                        <div className="row">
-                            <div className="col">Main Road, Block-K, South Banasree, Rampura, 1219 Dhaka (Bangladesh)
+                        <div className="d-flex">
+                            <div className="p-2"><i className="fa fa-map-marker"></i></div>
+                            <div className="p-2">Main Road, Block-K, South Banasree, Khilgoan-1219, Dhaka (Bangladesh)
                             </div>
                         </div>
-                        <div className="row">
-                            <div className="col">(+88)01710456520</div>
+                        <div className="d-flex">
+                            <div className="p-2"><i className="fa fa-mobile" aria-hidden="true"></i></div>
+                            <div className="p-2">(+88)01710456520</div>
                         </div>
-                        <div className="row">
-                            <div className="col">sonycsecu2@gmail.com</div>
+                        <div className="d-flex">
+                            <div className="p-2"><i className="fa fa-google" aria-hidden="true"></i></div>
+                            <div className="p-2">sonycsecu2@gmail.com</div>
                         </div>
-                        <div className="row">
-                            <div className="col">Skypee kamalhossainsony</div>
+                        <div className="d-flex">
+                            <div className="p-2"><i className="fa fa-skype" aria-hidden="true"></i></div>
+                            <div className="p-2">kamalhossainsony</div>
                         </div>
-                        <div className="row">
-                            <div className="col">https://www.hackerrank.com/Md_Kamal_Hossain</div>
+                        <div className="d-flex">
+                            <div className="p-2"><i className="fab fa-hackerrank"></i></div>
+                            <div className="p-2">https://www.hackerrank.com/Md_Kamal_Hossain</div>
                         </div>
-                        <div className="row">
-                            <div className="col">https://www.linkedin.com/in/md-kamal-hossain-71403513b/</div>
+                        <div className="d-flex">
+                            <div className="p-2"><i className="fa fa-linkedin" aria-hidden="true"></i></div>
+                            <div className="p-2">https://www.linkedin.com/in/md-kamal-hossain-71403513b/</div>
                         </div>
-                        <div className="row">
-                            <div className="col">Language: Bengali, English, Japanese, Hindi</div>
+                        <div className="d-flex">
+                            <div className="p-2"><i className="fa fa-comments" aria-hidden="true"></i></div>
+                            <div className="p-2">Language: Bengali, English, Japanese, Hindi</div>
                         </div>
                     </div>
                 </div>
