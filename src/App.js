68fa7b0d968c72ac08969a52f5765d59c181819a
kamal_hossain <sony@gmail.com>
2022-12-26 18:32:09 +0600

    SKILLS and some design correction

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import './custom_style.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
-import {fab} from '@fortawesome/free-brands-svg-icons';
 
 function App() {
     return (
@@ -21,7 +20,9 @@ function App() {
                     </div>
                     <div className="col-9">
                         <div className="d-flex flex-column">
-                            <div className="py-2 " style={{background: "#418ab3"}}><div className="mx-3">MD. KAMAL HOSSAIN</div></div>
+                            <div className="py-2 " style={{background: "#418ab3"}}>
+                                <div className="mx-3">MD. KAMAL HOSSAIN</div>
+                            </div>
                         </div>
                         <div className="d-flex">
                             <div className="p-2"><i className="fa fa-map-marker"></i></div>
@@ -72,9 +73,13 @@ function App() {
                             <div className="mt-1 fs-6 fw-bold" style={{color: "#418ab3"}}>SOFTWARE ENGINEER (15 MAR,
                                 2022 – TILL NOW)
                             </div>
-                            <div className="my-1" style={{color: "#418ab3"}}>Newgen Technology Limited,
-                                Dhaka(Bangladesh). web:
-                                <a href="https://newgen-bd.com/" className="link-primary">https://newgen-bd.com/</a>
+                            <div className="d-flex justify-content-start my-1">
+                                <div style={{color: "#418ab3"}}>Newgen Technology Limited,
+                                    Dhaka(Bangladesh).
+                                </div>
+                                <div className="mx-2">web:</div>
+                                <div><a href="https://newgen-bd.com/"
+                                        className="link-primary">https://newgen-bd.com/</a></div>
                             </div>
                             <div className="px-2"><p>Developing a new project names ”Agre-Science” which has to module
                                 like Inventory and
@@ -87,9 +92,13 @@ function App() {
                             <div className="mt-1 fs-6 fw-bold" style={{color: "#418ab3"}}>SOFTWARE ENGINEER (01 DEC,
                                 2020 – 14 MAR, 2022)
                             </div>
-                            <div className="my-1" style={{color: "#418ab3"}}>Datasoft Systems Bangladesh Limited,
-                                Dhaka(Bangladesh). web:
-                                <a href="https://datasoft-bd.com/" className="link-primary">https://datasoft-bd.com/</a>
+                            <div className="d-flex justify-content-start my-1">
+                                <div style={{color: "#418ab3"}}>Datasoft Systems Bangladesh Limited,
+                                    Dhaka(Bangladesh).
+                                </div>
+                                <div className="mx-2">web:</div>
+                                <div><a href="https://datasoft-bd.com/"
+                                        className="link-primary">https://datasoft-bd.com/</a></div>
                             </div>
                             <div className="px-2"><p>Developed Security service using OAuth2, routing service and
                                 service register using Eureka, Reporting service using jasper technology in spring boot.
@@ -102,9 +111,11 @@ function App() {
                             <div className="mt-1 fs-6 fw-bold" style={{color: "#418ab3"}}>SOFTWARE ENGINEER (01 DEC,
                                 2019 - 20 JUL, 2020)
                             </div>
-                            <div className="my-1" style={{color: "#418ab3"}}>Human Resocia, Shinjuku (Japan). web:
-                                <a href="https://git.resocia.jp/en/"
-                                   className="link-primary">https://git.resocia.jp/en/</a>
+                            <div className="d-flex justify-content-start my-1">
+                                <div style={{color: "#418ab3"}}>Human Resocia, Shinjuku (Japan).</div>
+                                <div className="mx-2">web:</div>
+                                <div><a href="https://git.resocia.jp/en/"
+                                        className="link-primary">https://git.resocia.jp/en/</a></div>
                             </div>
                             <div className="px-2"><p>I took Japanese Language (N4) and Culture Training to work as a
                                 Software Engineer in Japan under Human Resocia.</p>
@@ -115,9 +126,11 @@ function App() {
                             <div className="mt-1 fs-6 fw-bold" style={{color: "#418ab3"}}>SOFTWARE ENGINEER (01 SEP,
                                 2016 -24 NOV, 2019)
                             </div>
-                            <div className="my-1" style={{color: "#418ab3"}}>BRAC IT Services Limited, Dhaka
-                                (Bangladesh). web:
-                                <a href="http://www.bracits.com/" className="link-primary">http://www.bracits.com/</a>
+                            <div className="d-flex justify-content-start my-1">
+                                <div style={{color: "#418ab3"}}>BRAC IT Services Limited, Dhaka (Bangladesh).</div>
+                                <div className="mx-2">web:</div>
+                                <div><a href="http://www.bracits.com/"
+                                        className="link-primary">http://www.bracits.com/</a></div>
                             </div>
                             <div className="px-2">
                                 <p>This is the biggest ERP Solutions in Bangladesh History for BRAC
@@ -149,8 +162,11 @@ function App() {
                             <div className="mt-1 fs-6 fw-bold" style={{color: "#418ab3"}}>SOFTWARE ENGINEER (01 FEB,
                                 2016 - 31 AUG, 2016)
                             </div>
-                            <div className="my-1" style={{color: "#418ab3"}}>R-Creation, Chittagong (Bangladesh). web:
-                                <a href="http://rcreation-bd.com/" className="link-primary">http://rcreation-bd.com/</a>
+                            <div className="d-flex justify-content-start my-1">
+                                <div style={{color: "#418ab3"}}>R-Creation, Chittagong (Bangladesh).</div>
+                                <div className="mx-2">web:</div>
+                                <div><a href="http://rcreation-bd.com/"
+                                        className="link-primary">http://rcreation-bd.com/</a></div>
                             </div>
                             <div className="px-2">
                                 <p>This company has some products. Among them, I worked in a project named as
@@ -174,6 +190,65 @@ function App() {
                 {/*border*/}
                 <div className="row">
                     <div className="col-12 border-top my-3"></div>
+                </div>
+                {/*SKILLS*/}
+                <div className="row">
+                    <div className="col-3">
+                        <div className="d-flex justify-content-end">
+                            <div className="fs-5 fw-bold" style={{color: "#418ab3"}}>SKILLS</div>
+                        </div>
+                    </div>
+                    <div className="col-9">
+                        <div className="d-flex justify-content-start my-1">
+                            <div className="fw-bold">Language:</div>
+                            <div className="mx-2">Java 8, JavaScript, Groovy, Python, C</div>
+                        </div>
+                        <div className="d-flex justify-content-start my-1">
+                            <div className="fw-bold">Web & Scripting Language:</div>
+                            <div className="mx-2">GSP, JSP, HTML5, CSS3, JavaScript, jQuery, Ajax, Twitter Bootstrap,
+                                Thymeleaf
+                            </div>
+                        </div>
+                        <div className="d-flex justify-content-start my-1">
+                            <div className="fw-bold">Framework:</div>
+                            <div className="mx-2">Spring Boot, React JS, Node JS, , Vue JS, Grails, JPA, Hibernate</div>
+                        </div>
+                        <div className="d-flex justify-content-start my-1">
+                            <div className="fw-bold">Tools:</div>
+                            <div className="mx-2">IntelliJ IDEA, WebStorm, Datagrip, PyCharm, Eclipse, SQLyog, Studio3T,
+                                PostgreSQL Maestro, Postman
+                            </div>
+                        </div>
+                        <div className="d-flex justify-content-start my-1">
+                            <div className="fw-bold">Build Tool:</div>
+                            <div className="mx-2">Maven, Gradle</div>
+                        </div>
+                        <div className="d-flex justify-content-start my-1">
+                            <div className="fw-bold">Reporting Tools:</div>
+                            <div className="mx-2">iReport, Jasper Studio, Jasper Server</div>
+                        </div>
+                        <div className="d-flex justify-content-start my-1">
+                            <div className="fw-bold">Database:</div>
+                            <div className="mx-2">PostgreSQL, MySQL, MongoDB</div>
+                        </div>
+                        <div className="d-flex justify-content-start my-1">
+                            <div className="fw-bold">Version Control System & Host:</div>
+                            <div className="mx-2">Git, SVN, Tortoise SVN, GitHub, GitLab, Jira, Redmine</div>
+                        </div>
+                        <div className="d-flex justify-content-start my-1">
+                            <div className="fw-bold">Application Server:</div>
+                            <div className="mx-2">Apache Tomcat</div>
+                        </div>
+                        <div className="d-flex justify-content-start my-1">
+                            <div className="fw-bold">Operating System:</div>
+                            <div className="mx-2">Linux (Ubuntu 20.04), Windows 10</div>
+                        </div>
+
+                    </div>
+                </div>
+                {/*footer*/}
+                <div className="row">
+                    <div className="col-12 my-3"></div>
                 </div>
             </div>
 
